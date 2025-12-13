160cf930674a3ea25d2534d9687dabb54b2c07e5
mhjmaas <marcel.maas@alliander.com>
2025-12-13 12:29:10 +0100

    chore: update version

--- vibe/core/__init__.py
+++ vibe/core/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 __all__ = ["__version__", "run_programmatic"]
-__version__ = "1.1.2+local"
+__version__ = "1.1.3+local"
 
 from vibe.core.programmatic import run_programmatic
